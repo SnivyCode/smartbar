2b5c137db995876da6210dd2a477a11f5fc8d2c1
SnivyCode <71209068+SnivyCode@users.noreply.github.com>
2022-01-21 21:03:44 +0100

    Icons

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function onclick() {
+function click() {
     window.open("ordini/ordini.html", "_self")
+}
+
+
+function validate() {
+    var input = document.getElementById("input").value;
+    var password = "thomas"
+    
+    if(input==password) {
+        //Ok
+        window.open("ordini/ordini.html", "_self")
+        
+    } else alert("Password is not correct.")
 }
